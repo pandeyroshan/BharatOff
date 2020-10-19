055d483299a822235ee88539327d13dda30898f3
pandeyroshan <pandeyroshan556@gmail.com>
2020-10-19 22:52:21 +0530

    Added Shop register form

--- users/forms.py
+++ users/forms.py
@@ -9,3 +9,27 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username','email','password1','password2','phone']
+
+CITY_CHOICE = []
+
+all_city = CityData.objects.all()
+i = CityData.objects.all()[0].id
+
+for city in all_city:
+    CITY_CHOICE.append([i,city.city_name])
+    i+=1
+
+class ShopkeeperRegisterForm(UserCreationForm):
+    email = forms.EmailField()
+    phone = forms.CharField(max_length=15, required=False)
+    city = forms.ChoiceField(choices = CITY_CHOICE)
+    comment = forms.CharField()
+
+    def __init__(self, *args, **kwargs):
+        super(ShopkeeperRegisterForm, self).__init__(*args, **kwargs)
+        self.fields['email'].label = "Email Address"
+        self.fields['comment'].label = "Requirements/Expectations"
+
+    class Meta:
+        model = User
+        fields = ['username','email','password1','password2','city','phone','comment']
