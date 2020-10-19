77be9c997d2fd34eca385fd9f14b1da212aa8229
pandeyroshan <pandeyroshan556@gmail.com>
2020-10-19 22:51:01 +0530

    Model for Shopkeepers

--- users/models.py
+++ users/models.py
@@ -12,5 +12,19 @@ class UserProfile(models.Model):
         return self.user.username
     
     class Meta:
-        verbose_name = 'Profiles'
-        verbose_name_plural = 'Profiles'
+        verbose_name = 'All Profiles'
+        verbose_name_plural = 'All Profiles'
+
+class Shopkeeper(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    mobile_number = models.CharField(max_length=15, blank=False)
+    pwd = models.CharField('Password', max_length=1000, blank=False)
+    city = models.ForeignKey(CityData, blank=True, on_delete=models.CASCADE)
+    comment = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.user.username
+    
+    class Meta:
+        verbose_name = 'Shopkeepers Profile'
+        verbose_name_plural = 'Shopkeepers Profile'
