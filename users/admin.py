e4ff447863b491e31fce2ec73185a79ef4362d7b
pandeyroshan <pandeyroshan556@gmail.com>
2020-10-19 22:52:03 +0530

    Registered shopkeeper in Admin Panel

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import UserProfile
+from .models import UserProfile,Shopkeeper
+
+class ShopkeeperAdmin(admin.ModelAdmin):
+    model = Shopkeeper
+    list_display = ('user','mobile_number','pwd','city')
+    search_fields = ('user',)
+    list_filter = ('city',)
 
 admin.site.register(UserProfile)
+admin.site.register(Shopkeeper, ShopkeeperAdmin)
