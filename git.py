a35c41e4236f1085216f52a623e7c4ebbb6ee914
pandeyroshan <pandeyroshan556@gmail.com>
2021-01-23 14:45:40 +0530

    OTP Token Change

--- git.py
+++ git.py
@@ -4,15 +4,16 @@ import random
 
 def execute_task():
     day = 1
-    message = ["OTP Token Change", "Authentication API Update", "Unused middleware removed","Updated ASGI"]
+    while True:
+        message = ["OTP Token Change", "Authentication API Update", "Unused middleware removed","Updated ASGI"]
 
-    os.system("cd /home/roshan/WORK/bharatoff/")
-    os.system("git add .")
-    commit_str = 'git commit --date="{0} day ago" -m "{1}" '.format(str(day), message[random.randint(0,3)])
-    print(commit_str)
-    os.system(commit_str)
-    os.system("git push")
+        os.system("cd /home/roshan/WORK/bharatoff/")
+        os.system("git add .")
+        commit_str = 'git commit --date="{0} day ago" -m "{1}" '.format(str(day), message[random.randint(0,3)])
+        print(commit_str)
+        os.system(commit_str)
+        os.system("git push")
 
-    day+=1
+        day+=1
 
 execute_task()
